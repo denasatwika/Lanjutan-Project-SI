6e77f96c08ddff1dba321c142f52699a641c0f38
Gonlsali <radhita09@gmail.com>
2025-11-03 11:20:58 +0800

    fixed little bug

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -24,7 +24,7 @@ export default function AppShell({
     <div className="mx-auto max-w-7xl px-4 md:px-6 pt-6">
       <div className="md:grid md:grid-cols-[260px_minmax(0,1fr)] md:gap-6">
         <aside className="md:sticky md:top-6 md:self-start md:z-20">
-          <NavBar />
+          <NavBar role={role} />
         </aside>
 
         <main className="pb-24 md:pb-0 min-w-0">{children}</main>
